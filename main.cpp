31ed79bdc2f372717d776f0d45818598c06715c9
ubuntu <ubuntu@pa2.(none)>
2014-03-16 16:36:20 +0100

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: ubuntu
+ *
+ * Created on March 16, 2014, 4:33 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
